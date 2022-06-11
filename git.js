6504d1b26ab5ff6cfcfb1ecf05fb61aaacff1169
mohamadrido <54422386+mohamadrido@users.noreply.github.com>
2022-06-11 09:05:36 +0700

    Update git.js

--- git.js
+++ git.js
@@ -1,49 +1,49 @@
 /*! Linux Software */
-function blender(){window.open('https://blender.org');}
-function krita){window.open('https://krita.org');}
-function gimp(){window.open('https://gimp.org');}
-function inkscape(){window.open('https://inkscape.org');}
-function kdenlive(){window.open('https://kdenlive.org');}
+function blender_link(){window.open('https://blender.org');}
+function krita_link(){window.open('https://krita.org');}
+function gimp_link(){window.open('https://gimp.org');}
+function inkscape_link(){window.open('https://inkscape.org');}
+function kdenlive_link(){window.open('https://kdenlive.org');}
 
 
 /*! Platforms */
-function github(){window.open('https://pages.github.com');}
-function blogger(){window.open('https://blogger.com');}
-function wordpress(){window.open('https://wordpress.org');}
+function github_link(){window.open('https://pages.github.com');}
+function blogger_link(){window.open('https://blogger.com');}
+function wordpress_link(){window.open('https://wordpress.org');}
 
 
 /*! Programming Language */
-function html(){window.open('https://en.wikipedia.org/wiki/HTML5');}
-function css(){window.open('https://en.wikipedia.org/wiki/CSS');}
-function javascript(){window.open('https://javascript.com');}
-function jekyll(){window.open('https://jekyllrb.com');}
-function ruby(){window.open('https://ruby-lang.org');}
+function html_link(){window.open('https://en.wikipedia.org/wiki/HTML5');}
+function css_link(){window.open('https://en.wikipedia.org/wiki/CSS');}
+function javascript_link(){window.open('https://javascript.com');}
+function jekyll_link(){window.open('https://jekyllrb.com');}
+function ruby_link(){window.open('https://ruby-lang.org');}
 
 
 /*! Linux System Operating */
-function linux(){window.open('https://linux.org');}
-function tails(){window.open('https://tails.boum.org');}
-function debian(){window.open('https://debian.org');}
-function kali(){window.open('https://kali.org');}
-function ubuntu(){window.open('https://ubuntu.com');}
-function studio(){window.open('https://ubuntustudio.org');}
-function mint(){window.open('https://linuxmint.com');}
-function backtrack(){window.open('https://backtrack-linux.org');}
-function slackware(){window.open('http://slackware.com');}
+function linux_link(){window.open('https://linux.org');}
+function tails_link(){window.open('https://tails.boum.org');}
+function debian_link(){window.open('https://debian.org');}
+function kali_link(){window.open('https://kali.org');}
+function ubuntu_link(){window.open('https://ubuntu.com');}
+function studio_link(){window.open('https://ubuntustudio.org');}
+function mint_link(){window.open('https://linuxmint.com');}
+function backtrack_link(){window.open('https://backtrack-linux.org');}
+function slackware_link(){window.open('http://slackware.com');}
 
 /*! Windows Software */
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
-function linux(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
+function linux_link(){window.open('https://linux.org');}
 
 /*! Windows System Operating */
-function xp(){window.open('https://support.microsoft.com/en-us/windows/windows-xp-support-has-ended-47b944b8-f4d3-82f2-9acc-21c79ee6ef5e');}
-function vista(){window.open('https://www.microsoft.com/en-us/download/details.aspx?id=13158');}
-function seven(){window.open('https://www.microsoft.com/en-US/windows/windows-7-end-of-life-support-information');}
-function ten(){window.open('https://www.microsoft.com/en-us/software-download/windows10ISO');}
+function xp_link(){window.open('https://support.microsoft.com/en-us/windows/windows-xp-support-has-ended-47b944b8-f4d3-82f2-9acc-21c79ee6ef5e');}
+function vista_link(){window.open('https://www.microsoft.com/en-us/download/details.aspx?id=13158');}
+function seven_link(){window.open('https://www.microsoft.com/en-US/windows/windows-7-end-of-life-support-information');}
+function ten_link(){window.open('https://www.microsoft.com/en-us/software-download/windows10ISO');}
